2bb7068b7ed89b77ec9b4b62092aeb8fdc583718
John Mount <jmount@win-vector.com>
2013-02-09 14:23:50 -0800

    load multiple files entry point

---
+++ src/com/winvector/db/LoadFiles.java
@@ -0,0 +1,50 @@
+package com.winvector.db;
+
+import java.io.File;
+import java.net.URI;
+import java.util.Date;
+
+import com.winvector.db.DBUtil.DBHandle;
+import com.winvector.util.BurstMap;
+import com.winvector.util.TrivialReader;
+
+public class LoadFiles {
+	public static void main(final String[] args) throws Exception {
+		final Date now = new Date();
+		System.out.println("start LoadFiles\t" + now);
+		final int firstSourceArg = 3;
+		if(args.length<4) {
+			throw new Exception("use: LoadFiles dbProbsURI sepChar tableName inputURI+");
+		}
+		final URI propsURI = new URI(args[0]);
+		final char sep = args[1].charAt(0);
+		final String tableName = args[2];
+		System.out.println("\tcwd: " + (new File(".")).getAbsolutePath());
+		System.out.println("\tDBProperties XML:\t" + propsURI.toString());
+		System.out.println("\tsep: " + sep);
+		System.out.println("\ttableName:\t" + tableName);
+		final DBHandle handle = DBUtil.buildConnection(propsURI,false);
+		System.out.println("\tdb:\t" + handle);
+		final TableControl tableControl = new TableControl(tableName);
+		for(int argi=firstSourceArg;argi<args.length;++argi) {
+			final URI inURI = new URI(args[argi]);
+			System.out.println("\tscan source URI:\t" + inURI);
+			final Iterable<BurstMap> source = new TrivialReader(inURI,sep,null,true,null, false);
+			tableControl.scanForDefs(inURI.toString(),source, null);
+		}
+		tableControl.buildSQLStatements();
+		tableControl.createTable(handle);
+		long nInserted = 0;
+		for(int argi=firstSourceArg;argi<args.length;++argi) {
+			final URI inURI = new URI(args[argi]);
+			System.out.println("\tload source URI:\t" + inURI);
+			final Iterable<BurstMap> source = new TrivialReader(inURI,sep,null,true,null, false);
+			final long nI = tableControl.loadData(inURI.toString(),now,source, null, handle);
+			System.out.println("\tinserted " + nI + " rows");
+			nInserted += nI;
+		}
+		handle.conn.close();
+		System.out.println("\tdone, wrote " + nInserted + "rows\t" + new Date());
+		System.out.println("done LoadFiles\t" + new Date());
+	}
+}
--- src/com/winvector/db/LoadTable.java
+++ src/com/winvector/db/LoadTable.java
@@ -2,24 +2,25 @@ package com.winvector.db;
 
 import java.io.File;
 import java.net.URI;
-import java.sql.SQLException;
 import java.util.Date;
 
 import com.winvector.db.DBUtil.DBHandle;
 import com.winvector.util.BurstMap;
-import com.winvector.util.RowCritique;
 import com.winvector.util.TrivialReader;
 
 
 public class LoadTable {
 	
 	public static void main(final String[] args) throws Exception {
+		final Date now = new Date();
+		System.out.println("start LoadTable\t" + now);
+		if(args.length!=4) {
+			throw new Exception("use: LoadTable dbProbsURI sepChar inputURI tableName");
+		}
 		final URI propsURI = new URI(args[0]);
 		final char sep = args[1].charAt(0);
 		final URI inURI = new URI(args[2]);
 		final String tableName = args[3];
-		
-		System.out.println("start LoadTable\t" + new Date());
 		System.out.println("\tcwd: " + (new File(".")).getAbsolutePath());
 		System.out.println("\tDBProperties XML:\t" + propsURI.toString());
 		System.out.println("\tsep: " + sep);
@@ -29,21 +30,14 @@ public class LoadTable {
 		System.out.println("\tdb:\t" + handle);
 		
 		final Iterable<BurstMap> source = new TrivialReader(inURI,sep,null,true,null, false);
-		loadTable(inURI.toString(),source, null, tableName, handle);
-		handle.conn.close();
-		
-		System.out.println("done LoadTable\t" + new Date());
-	}
-	
-	
-	public static void loadTable(final String sourceName, final Iterable<BurstMap> source, final RowCritique gateKeeper,
-			final String tableName, final DBHandle handle) throws SQLException {
-		final Date now = new Date();
+		String sourceName = inURI.toString();
 		final TableControl tableControl = new TableControl(tableName);
-		tableControl.scanForDefs(sourceName,source, gateKeeper);
+		tableControl.scanForDefs(sourceName,source, null);
 		tableControl.buildSQLStatements();
 		tableControl.createTable(handle);
-		final long nInserted = tableControl.loadData(sourceName,now,source, gateKeeper, handle);
-		System.out.println("\tdone, wrote " + nInserted + "\t" + new Date());
+		final long nInserted = tableControl.loadData(sourceName,now,source, null, handle);
+		System.out.println("\tdone, wrote " + nInserted + " rows\t" + new Date());
+		handle.conn.close();
+		System.out.println("done LoadTable\t" + new Date());
 	}
 }
